4900993dbc5d1428b4105f3ddb7abc6fdf20e6b9
Lucas Nolasco Ynoguti <lucas.nolasco-ynoguti@etu.univ-grenoble-alpes.fr>
2025-12-29 08:39:55 +0000

    update: added new content panel and card layout for better screen management

--- src/main/java/io/github/lucasynoguti/ui/RootPanel.java
+++ src/main/java/io/github/lucasynoguti/ui/RootPanel.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class RootPanel extends JPanel {
-
+    private final JPanel contentPanel;
     private final CardLayout cardLayout;
 
     public static final String POMODORO = "POMODORO";
@@ -12,20 +12,25 @@ public class RootPanel extends JPanel {
     public static final String CATS = "CATS";
 
     public RootPanel() {
-        cardLayout = new CardLayout();
-        setLayout(cardLayout);
+        setLayout(new BorderLayout());
         setOpaque(true);
+
+        cardLayout = new CardLayout();
+        contentPanel = new JPanel(cardLayout);
+        contentPanel.setOpaque(false);
+
+        add(contentPanel, BorderLayout.CENTER);
     }
 
     public void addScreen(JPanel panel, String name) {
-        JPanel container = new JPanel(new GridBagLayout());
-        container.setOpaque(false);
-        container.add(panel);
-        add(container, name);
+        JPanel wraper = new JPanel(new GridBagLayout());
+        wraper.setOpaque(false);
+        wraper.add(panel);
+        contentPanel.add(wraper, name);
     }
 
     public void showScreen(String name) {
-        cardLayout.show(this, name);
+        cardLayout.show(contentPanel, name);
     }
 
     public void updateBackground(Color color) {
